3438a22f113c10d569b58314dbf403243a188f7e
raman04-byte <ramantank04022002@gmail.com>
2024-03-23 16:45:17 +0530

    New Update

--- lib/main.dart
+++ lib/main.dart
@@ -20,3 +20,11 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// import 'package:appwrite/appwrite.dart';
+
+// Client client = Client();
+// client
+//     .setEndpoint('http://54.242.44.19/v1')
+//     .setProject('65f31e0dee7417a5dc36')
+//     .setSelfSigned(status: true); 
